e0315fdcf38eb90172e3191da4a50e505a9a5959
Diegoweh <diegocelaia@outlook.com>
2025-03-11 10:45:27 -0700

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { WhyComponent } from './components/why/why.component';
+import { SelectProductComponent } from './components/select-product/select-product.component';
+import { IndustriesComponent } from './components/industries/industries.component';
+import { ServicezComponent } from './components/servicez/services.component';
+import { ClientsComponent } from './components/clients/clients.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'why', component: WhyComponent },
+  { path: 'select-product', component: SelectProductComponent },
+  { path: 'industries', component: IndustriesComponent },
+  { path: 'services', component: ServicezComponent },
+  { path: 'clients', component: ClientsComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
