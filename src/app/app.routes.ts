013e07419e7d21dc6c9be3c8c25e1888bf282649
Diegoweh <diegocelaia@outlook.com>
2025-05-09 11:34:41 -0700

    suppost last details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
-import { WhyComponent } from './components/why/why.component';
 import { SelectProductComponent } from './components/select-product/select-product.component';
 import { IndustriesComponent } from './components/industries/industries.component';
 import { ServicezComponent } from './components/servicez/services.component';
